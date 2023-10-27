2ca9ccb61210a4715276e19f377eb8483b405e4c
samirRamos039 <rzagiza039@gmail.com>
2023-10-26 21:41:49 -0500

    probando la conexion

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,12 +14,49 @@
               <th>APODO</th>
             </tr>
           </thead>
-  
-         </table>
+          <tbody class="table-group-divider" id="contenido"></tbody>
+           <tr v-if="this.cargando">
+            <td colspan="5"><h3>CARGANDO...</h3></td>
+          </tr>
+          <tr v-else v-for="user, i in this.usuario" :key ="user.id">
+            <td v-text="user.id"></td>
+            <td v-text="user.email"></td>
+            <td v-text="user.foto"></td>
+            <td v-text="user.nombre"></td>
+            <td v-text="user.apodo"></td>
+          </tr>
+        </table>
 
       </div>
     </div>
-    
-
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default{
+  data(){
+    return{
+
+      usuario:null,
+      cargando:false
+
+    }
+  },
+  mounted(){
+    this.getUser
+  },
+  methods:{
+    getUser(){
+       this.cargando = true;
+       axios.get('http://localhost:8000/api/v1/Usuario').then(
+        res =>{
+          this.usuario = res.data;
+          this.cargando = false;
+        }
+       )
+    }
+  }
+}
+</script>
