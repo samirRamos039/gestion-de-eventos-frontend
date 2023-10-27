e030a401cf7d13d05394b381cf7391f349b2c167
samirRamos039 <rzagiza039@gmail.com>
2023-10-26 20:22:40 -0500

    interfaz de usuario

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './assets/main.css'
-
+import 'bootstrap/dist/css/bootstrap.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import App from './App.vue'
 import router from './router'
 
